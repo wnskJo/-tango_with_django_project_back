c59e65e36ade14da4cffdd2e10b2e455e04786d2
Wonseok Cho <wonseokcho777@gmail.com>
2019-01-25 12:03:14 +0000

    Chapter 4 Exercises

--- rango/views.py
+++ rango/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return render(request, 'rango/about.html')
